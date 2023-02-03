4b77587c105eb66d6c0a012ec77d97cfbf877cb9
Luke Reynolds <lukereynoldspi@gmail.com>
2023-02-03 14:40:53 -0800

    added logo and home page to navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Nav, NavLink, NavMenu } 
     from "./navbar-elements";
+import { ReactComponent as MealSpotterLogo } from "../../assets/pictures/mealspotter.svg";
   
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavMenu>
+        <MealSpotterLogo/>
+        <NavLink to="" activeStyle>
+            Home
+          </NavLink>
           <NavLink to="/about" activeStyle>
             About
           </NavLink>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to MealSpotter</h1>
+      <h1>Welcome to Meal Spotter!</h1>
     </div>
   );
 };
