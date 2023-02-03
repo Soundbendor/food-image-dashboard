11ed6160fef865bcee293b80fd6c9f36ae70810d
Luke Reynolds <lukereynoldspi@gmail.com>
2023-02-03 13:59:58 -0800

    implemented baisc navbar system

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
 import React, { Component }  from 'react';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The one pieeeece the one piece is reeeeeeeal!!!!
-        </p>
-      </header>
-    </div>
+      <Router>
+      <Navbar />
+      <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/about' element={<About/>} />
+      </Routes>
+      </Router>
   );
-}
-
-export default App;
+  }
+    
+  export default App;
