54b45fc0c4be7fd6c65b3a40bd8912f43fd69db5
lukereynoldspi <lukereynoldspi@gmail.com>
2023-02-03 22:13:24 -0800

    fixed orientation of navbar

--- src/components/navbar/navbar-elements.js
+++ src/components/navbar/navbar-elements.js
@@ -3,16 +3,15 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
   
 export const Nav = styled.nav`
-  background: #ffb3ff;
+  background: #645CBB;
   height: 85px;
   display: flex;
   justify-content: space-between;
-  padding: 0.2rem calc((100vw - 1000px) / 2);
   z-index: 12;
 `;
   
 export const NavLink = styled(Link)`
-  color: #808080;
+  color: #000000;
   display: flex;
   align-items: center;
   text-decoration: none;
