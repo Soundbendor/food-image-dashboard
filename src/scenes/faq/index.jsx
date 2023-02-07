b3d36ac5956ac433d0cb465e0f74ead5e1c4099e
Luke Reynolds <lukereynoldspi@gmail.com>
2023-02-06 18:34:55 -0800

    edited faq page, testing new table features

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,20 +1,105 @@
-import { Box, useTheme } from "@mui/material";
-import Header from "../../components/Header";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import React from "react";
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
+import { mockDataPatients } from "../../data/mockData";
+import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
+
+
 
 const FAQ = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const handleRowEditCommit = React.useCallback(
+    (params) => {
+        const id = params.id;
+        const key = params.field;
+        const value = params.value; },
+    []
+  );
+
+  const columns = [
+    { field: "id", 
+    headerName: "ID", 
+    flex: 0.5 , 
+    cellClassName: "name-column--cell",
+    },
+    {
+      field: "first_name",
+      headerName: "First",
+      flex: 1,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+      editable: true,
+    },
+    {
+      field: "last_name",
+      headerName: "Last",
+      flex: 1,
+      cellClassName: "name-column--cell",
+      editable: true,
+    },
+    {
+      field: "username",
+      headerName: "Username",
+      headerAlign: "left",
+      align: "left",
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "userhash",
+      headerName: "Userhash",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+  ];
+
   return (
     <Box m="20px">
-      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+      <Header
+        title="FAQ"
+      />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: "#000000",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: "#ced4da",
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+          },
+          "& .MuiCheckbox-root": {
+            color: "#000000",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: "#000000",
+          },
+        }}
+      >
+        <DataGrid
+          rows={mockDataPatients}
+          columns={columns}
+          onCellEditCommit={handleRowEditCommit}
+          components={{ Toolbar: GridToolbar }}
+        />
+      </Box>
     </Box>
   );
 };
 
 export default FAQ;
+
