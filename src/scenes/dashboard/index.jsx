b6ee0e75549d366f0226d892a5fbfd99ff0f6665
lukereynoldspi <lukereynoldspi@gmail.com>
2023-02-06 20:02:31 -0800

    edited dashboard charts to reflect the design

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -3,7 +3,7 @@ import { tokens } from "../../theme";
 import { mockTransactions } from "../../data/mockData";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 import EmailIcon from "@mui/icons-material/Email";
-import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import FastfoodIcon from '@mui/icons-material/Fastfood';
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
 import Header from "../../components/Header";
@@ -23,6 +23,7 @@ const Dashboard = () => {
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
 
         <Box>
+        <a href="../../data/mockData.js" download="">
           <Button
             sx={{
               backgroundColor: colors.blueAccent[700],
@@ -32,9 +33,12 @@ const Dashboard = () => {
               padding: "10px 20px",
             }}
           >
+            
             <DownloadOutlinedIcon sx={{ mr: "10px" }} />
             Download Reports
+            
           </Button>
+          </a>
         </Box>
       </Box>
 
@@ -54,7 +58,7 @@ const Dashboard = () => {
           justifyContent="center"
         >
           <StatBox
-            title="12,361"
+            title="61"
             subtitle="Emails Sent"
             progress="0.75"
             increase="+14%"
@@ -73,12 +77,10 @@ const Dashboard = () => {
           justifyContent="center"
         >
           <StatBox
-            title="431,225"
-            subtitle="Sales Obtained"
-            progress="0.50"
-            increase="+21%"
+            title="889"
+            subtitle="Meals in Database"
             icon={
-              <PointOfSaleIcon
+              <FastfoodIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -92,31 +94,10 @@ const Dashboard = () => {
           justifyContent="center"
         >
           <StatBox
-            title="32,441"
-            subtitle="New Clients"
-            progress="0.30"
-            increase="+5%"
+            title="441"
+            subtitle="Patients"
             icon={
               <PersonAddIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="1,325,134"
-            subtitle="Traffic Received"
-            progress="0.80"
-            increase="+43%"
-            icon={
-              <TrafficIcon
                 sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
               />
             }
@@ -142,14 +123,14 @@ const Dashboard = () => {
                 fontWeight="600"
                 color={colors.grey[100]}
               >
-                Revenue Generated
+                Recent Patient Food Intake
               </Typography>
               <Typography
                 variant="h3"
                 fontWeight="bold"
                 color={colors.greenAccent[500]}
               >
-                $59,342.32
+                Past 30 Days
               </Typography>
             </Box>
             <Box>
@@ -216,32 +197,7 @@ const Dashboard = () => {
         </Box>
 
         {/* ROW 3 */}
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          p="30px"
-        >
-          <Typography variant="h5" fontWeight="600">
-            Campaign
-          </Typography>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            mt="25px"
-          >
-            <ProgressCircle size="125" />
-            <Typography
-              variant="h5"
-              color={colors.greenAccent[500]}
-              sx={{ mt: "15px" }}
-            >
-              $48,352 revenue generated
-            </Typography>
-            <Typography>Includes extra misc expenditures and costs</Typography>
-          </Box>
-        </Box>
+          
         <Box
           gridColumn="span 4"
           gridRow="span 2"
@@ -252,7 +208,7 @@ const Dashboard = () => {
             fontWeight="600"
             sx={{ padding: "30px 30px 0 30px" }}
           >
-            Sales Quantity
+            Daily Patient Food Intake
           </Typography>
           <Box height="250px" mt="-20px">
             <BarChart isDashboard={true} />
