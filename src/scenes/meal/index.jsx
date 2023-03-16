61b36ad591a2dbf10cb9cf6492c04e03edadeeb2
Ubuntu <ubuntu@ip-172-31-26-47.us-west-2.compute.internal>
2023-03-16 17:34:00 +0000

    display food data in meal page

--- src/scenes/meal/index.jsx
+++ src/scenes/meal/index.jsx
@@ -15,172 +15,135 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import TablePagination from '@mui/material/TablePagination';
 import Axios from 'axios'
+import {useState,useEffect} from 'react'
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import Header from "../../components/Header";
 
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    history: [
-      {
-        date: '2020-01-05',
-        time: '1:00pm',
-        amount: 3,
-      },
-      {
-        date: '2020-01-02',
-        time: '6:00am',
-        amount: 1,
-      },
-    ],
-  };
-}
-
-function Row(props) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
-  return (
-    <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        <TableCell component="th" scope="row">
-          {row.name}
-        </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Time</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.time}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
-  );
-}
-
-Row.propTypes = {
-  row: PropTypes.shape({
-    calories: PropTypes.number.isRequired,
-    carbs: PropTypes.number.isRequired,
-    fat: PropTypes.number.isRequired,
-    history: PropTypes.arrayOf(
-      PropTypes.shape({
-        amount: PropTypes.number.isRequired,
-        time: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-      }),
-    ).isRequired,
-    name: PropTypes.string.isRequired,
-    protein: PropTypes.number.isRequired,
-  }).isRequired,
-};
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-
-];
-
-const tableContainerSx = {
-    border: "1px solid rgba(128,128,128,0.4)",
-    width: "80%",
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginTop: 6,
-    borderRadius: 2,
-    height: "75%",
-  };
 const Meal = () => {
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  
-    const handleChangePage = (event, newPage) => {
-      setPage(newPage);
-    };
-  
-    const handleChangeRowsPerPage = (event) => {
-      setRowsPerPage(+event.target.value);
-      setPage(0);
-    };
-  
-    return (
-    
-    <TableContainer component={Paper} sx={tableContainerSx}>
-        
-      <Table aria-label="collapsible table" stickyHeader>
-        <TableHead >
-          <TableRow >
-            <TableCell style={{ backgroundColor: '#71797E' }}/>
-            <TableCell style={{ backgroundColor: '#71797E' }} >Meal</TableCell>
-            <TableCell style={{ backgroundColor: '#71797E' }} align="right">Calories</TableCell>
-            <TableCell style={{ backgroundColor: '#71797E' }} align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell style={{ backgroundColor: '#71797E' }} align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell style={{ backgroundColor: '#71797E' }} align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <Row key={row.name} row={row} />
-          ))}
-        </TableBody>
-      </Table>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+    const [foodList,setFoodList]=useState([]);
+      const columns = [
+    { field: "FoodID",
+    headerName: "ID",
+    flex: 0.5 ,
+    cellClassName: "name-column--cell",
+    },
+    {
+      field: "FoodName",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+    },
+    {
+      field: "SugarPerServ",
+      headerName: "Sugar(g)",
+      flex: 0.5,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+    },
+    {	
+      field: "SodiumPerServ",
+      headerName: "Sodium(mg)",
+      flex: 0.5,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+    },
+    {
+      field: "Calories",
+      headerName: "Calories",
+      flex: 0.5,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+    },
+    {
+      field: "ProteinPerServ",
+      headerName: "Protein(g)",
+      flex: 0.5,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+    },
+    {
+      field: "FatPerServ",
+      headerName: "Fat(g)",
+      flex: 0.5,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+    }, 
+    {
+      field: "SatFatPerServ",
+      headerName: "Saturated Fat(g)",
+      flex: 0.5,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+    },
+    {
+      field: "TransFatPerServ",
+      headerName: "Trans Fat(g)",
+      flex: 0.5,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+    },
+    {
+      field: "CarbPerServ",
+      headerName: "Carbonate(g)",
+      flex: 0.5,
+      cellClassName: "name-column--cell",
+      color: "#000000",
+    },
+      ]
+    useEffect(()=>{
+      Axios.get("http://ec2-54-203-249-218.us-west-2.compute.amazonaws.com:3002/DBApi/getFoods").then((data)=>{
+        console.log(data)
+        setFoodList(data.data)
+      });
+    },[]);
+    console.log(foodList);
+	return (
+	 <Box m="20px">
+      <Header
+        title="Meal"
       />
-    </TableContainer>
-  );
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: "#000000",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: "#ced4da",
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+          },
+          "& .MuiCheckbox-root": {
+            color: "#000000",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: "#000000",
+          },
+        }}
+      >
+        <DataGrid
+          getRowId={(row) => row.FoodID}
+          rows={foodList}
+          columns={columns}
+          components={{ Toolbar: GridToolbar }}
+   />
+      </Box>
+    </Box>
+	);
+
 }
 export default Meal;
